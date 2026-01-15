f63a0da1c8c39a5989351efd5ebc35cb1f4e9b7b
ahmed <mujtabaaliahmed559@gmail.com>
2026-01-15 22:06:45 +0530

    Removed Unnecessary lines

--- lib/invoice.dart
+++ lib/invoice.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
-import 'models.dart';
 import 'widgets.dart';
 import 'theme.dart';
 
