9bb3917bcccccbbcc07651c169d036c295a22f9d
ahmed <mujtabaaliahmed559@gmail.com>
2026-01-23 23:52:54 +0530

    Added Service_item file

--- lib/models/service_item.dart
+++ lib/models/service_item.dart
@@ -1,26 +1,26 @@
 class ServiceItem {
-  final String id;
-  final String workItemId;
+  final String serviceid;
+//  final String workItemId;
   final String name;
   final double amount;
 
   ServiceItem({
-    required this.id,
-    required this.workItemId,
+    required this.serviceid,
+ //   required this.workItemId,
     required this.name,
     required this.amount,
   });
 
   Map<String, dynamic> toMap() => {
-        'id': id,
-        'workItemId': workItemId,
+        'serviceid': serviceid,
+    //    'workItemId': workItemId,
         'name': name,
         'amount': amount,
       };
 
   static ServiceItem fromMap(Map<String, dynamic> m) => ServiceItem(
-        id: (m['id'] ?? '').toString(),
-        workItemId: (m['workItemId'] ?? '').toString(),
+        serviceid: (m['serviceid'] ?? '').toString(),
+   //     workItemId: (m['workItemId'] ?? '').toString(),
         name: (m['name'] ?? '').toString(),
         amount: (m['amount'] is num)
             ? (m['amount'] as num).toDouble()
