9ad9ce08276f4fe834256ecb5184719d104278ad
ahmed <mujtabaaliahmed559@gmail.com>
2026-01-23 23:51:43 +0530

    Added basic url and endpoints

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,8 +1,11 @@
 class ApiConfig {
   // TODO: Replace with your actual API base URL
   static const String baseUrl = 'http://3.208.90.92:3000';
-  
+
   // API Endpoints
   static const String loginEndpoint = '/auth/login';
-  // Add more endpoints as needed
+  static const String createWorkItemEndpoint = '/workertaskui'; 
+  static const String serviceid = '/services';
+  static const String checkCustomerEndpoint = '/customers/check';
+    static const String emptask = "/workertaskui/b3b1f0ef-0a18-4583-b0bd-f8053db2d26f/2024-10-01/active";//change active to completed to get completed tasks
 }
