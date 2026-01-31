2a0576f63d4c9e7775fe40138eea74c886418d1a
ahmed <mujtabaaliahmed559@gmail.com>
2026-01-31 18:58:05 +0530

    added end point

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -5,7 +5,7 @@ class ApiConfig {
   // API Endpoints
   static const String loginEndpoint = '/auth/login';
   static const String createWorkItemEndpoint = '/workertaskui'; 
-  static const String serviceid = '/services';
+  static const String getservices = '/services';
   static const String checkCustomerEndpoint = '/customers/check';
     static const String emptask = "/workertaskui/b3b1f0ef-0a18-4583-b0bd-f8053db2d26f/2024-10-01/active";//change active to completed to get completed tasks
 }
